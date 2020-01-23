e406fa6dec5d0c102eb10b090aa39b7ce27d3bd0
Natalya Alexinskaya <150191@mail.ru>
2020-01-23 18:57:00 +0300

    new commit

--- src/com/company/CheckValidation.java
+++ src/com/company/CheckValidation.java
@@ -4,7 +4,7 @@ import com.exception.InvalidOperationException;
 import com.exception.WrongCharacterException;
 
 public class CheckValidation {
-    public static void check(String text) throws WrongCharacterException, InvalidOperationException {
+    public static void check(String text) {
         if (!text.matches("(?:\\s*\\d+(.\\d+)?\\s*[*+/-]\\s*)+\\d+(.\\d+)?")) {
             throw new WrongCharacterException("Вводите только положительные числа(целые или дробные, используя в качестве разделителя точку) и знаки математических операций(+-/*), не дублируя их!");
         } else if (text.contains("/0") || text.contains("/ 0")) {
